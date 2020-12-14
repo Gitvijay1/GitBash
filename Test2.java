69b619edd05b0d95cc7eafc66c637c81d77ae114
Vijay Sharma <vijay>
2020-12-14 09:00:04 +0530

    fifth commit

--- Test2.java
+++ Test2.java
@@ -1,6 +1,7 @@
 public class Test2{
     public static void main(String[] args){
         System.out.println("good morning");
-        System.out.println("good night");
+        
+    int a=10;
 }
 }
